f29a2c09af9492f9033dc13c6f14d010340651ba
Angela Chen <angelajiaqichen@gmail.com>
2022-09-14 15:40:05 -0400

    Initial Commit

---
+++ TempConverterApp/TempConverterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempConverterAppApp.swift
+//  TempConverterApp
+//
+//  Created by Angela Chen on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TempConverterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
